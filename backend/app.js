e091769e348abc1e1a37877a15441628aca6e80f
Daniel Lee <dlee13195@gmail.com>
2023-02-20 17:05:04 -0500

    Users model

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,16 @@
+const debug = require('debug');
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 
-const usersRouter = require('./routes/api/users'); // update the import file path
-const pagesRouter = require('./routes/api/pages');
-const csrfRouter = require('./routes/api/csrf');
 const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
+
+const usersRouter = require('./routes/api/users'); // update the import file path
+const pagesRouter = require('./routes/api/pages');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -44,6 +46,29 @@ app.use(
 app.use('/api/users', usersRouter); // update the path
 app.use('/api/pages', pagesRouter);
 app.use('/api/csrf', csrfRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
+
+module.exports = app;
 
 
 module.exports = app;

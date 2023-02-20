583acba85ff8836128f08f1824798c03a183b29f
Wilson Wu <wuyecheng738@gmail.com>
2023-02-20 12:00:42 -0500

    setup csrf and cors

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,10 @@ const logger = require('morgan');
 
 const usersRouter = require('./routes/api/users'); // update the import file path
 const pagesRouter = require('./routes/api/pages');
+const csrfRouter = require('./routes/api/csrf');
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
 
 const app = express();
 
@@ -13,9 +17,33 @@ app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 
+// ADD THIS SECURITY MIDDLEWARE
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, the Express 
+  // server will serve the React files statically.)
+  app.use(cors());
+}
+
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+
+
 // Attach Express routers
 
 app.use('/api/users', usersRouter); // update the path
 app.use('/api/pages', pagesRouter);
+app.use('/api/csrf', csrfRouter);
+
 
 module.exports = app;
--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI
+    mongoURI: process.env.MONGO_URI,
+    isProduction: process.env.NODE_ENV === 'production'
   }

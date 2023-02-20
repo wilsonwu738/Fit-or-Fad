d3ab2b8c22bee9027f34d9a22c93eb92a4111a44
Timothy Chang <tchang135@gmail.com>
2023-02-20 17:00:06 -0500

    add seed data for users

---
+++ backend/seeders/seeds.js
@@ -0,0 +1,99 @@
+const mongoose = require("mongoose");
+const { mongoURI: db } = require('../config/keys.js');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { faker } = require('@faker-js/faker');
+
+const NUM_SEED_USERS = 10;
+
+// Create users
+const users = [];
+
+users.push(
+  new User ({
+    username: 'demo-user',
+    email: 'demo-user@appacademy.io',
+    hashedPassword: bcrypt.hashSync('123456', 10)
+  })
+)
+
+users.push(
+    new User ({
+      username: 'dantheman',
+      email: 'dan@gmail.com',
+      hashedPassword: bcrypt.hashSync('123456', 10)
+    })
+)
+
+users.push(
+    new User ({
+      username: 'wilsonwonder',
+      email: 'willy@gmail.com',
+      hashedPassword: bcrypt.hashSync('123456', 10)
+    })
+)
+
+users.push(
+    new User ({
+      username: 'JQmodel',
+      email: 'jiong@gmail.com',
+      hashedPassword: bcrypt.hashSync('123456', 10)
+    })
+)
+
+users.push(
+    new User ({
+      username: 'chakybara',
+      email: 'chak@gmail.com',
+      hashedPassword: bcrypt.hashSync('123456', 10)
+    })
+)
+
+users.push(
+    new User ({
+      username: 'timmyturner',
+      email: 'tim@gmail.com',
+      hashedPassword: bcrypt.hashSync('123456', 10)
+    })
+)
+
+for (let i = 1; i < NUM_SEED_USERS; i++) {
+    const firstName = faker.name.firstName();
+    const lastName = faker.name.lastName();
+    users.push(
+      new User ({
+        username: faker.internet.userName(firstName, lastName),
+        email: faker.internet.email(firstName, lastName),
+        hashedPassword: bcrypt.hashSync(faker.internet.password(), 10)
+      })
+    )
+  }
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+    insertSeeds();
+  })
+  .catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+  });
+
+// Reset and seed db
+const insertSeeds = () => {
+  console.log("Resetting db and seeding users and tweets...");
+
+  User.collection.drop()
+                 .then(() => Tweet.collection.drop())
+                 .then(() => User.insertMany(users))
+                 .then(() => Tweet.insertMany(tweets))
+                 .then(() => {
+                   console.log("Done!");
+                   mongoose.disconnect();
+                 })
+                 .catch(err => {
+                   console.error(err.stack);
+                   process.exit(1);
+                 });
+}

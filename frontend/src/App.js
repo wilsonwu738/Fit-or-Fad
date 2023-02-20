2d06d8d4e6da9a0823fb0b7e5ea0a42413a3c3e5
Jiong Qi <jiongqipan@gmail.com>
2023-02-20 13:46:43 -0500

    Create Routes and MainPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,21 @@
+import { Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+import NavBar from './components/NavBar/NavBar';
+
+import MainPage from './components/MainPage/MainPage';
+import LoginForm from './components/SessionForms/LoginForm';
+import SignupForm from './components/SessionForms/SignupForm';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <NavBar />
+      <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/login" component={LoginForm} />
+        <AuthRoute exact path="/signup" component={SignupForm} />
+      </Switch>
+    </>
   );
 }
 

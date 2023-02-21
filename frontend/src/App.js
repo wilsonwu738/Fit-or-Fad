4dbe259c33a59fcfa8eefab547533baf81bc48b7
Jiong Qi <jiongqipan@gmail.com>
2023-02-20 22:31:51 -0500

    Add Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 import { Switch } from 'react-router-dom';
 
 import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
-// import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/NavBar/NavBar';
 
 import MainPage from './components/MainPage/MainPage';
 import LoginForm from './components/SessionForms/LoginForm';
@@ -20,7 +20,7 @@ function App() {
 
   return loaded && (
     <>
-      {/* <NavBar /> */}
+      <NavBar />
       <Switch>
         <AuthRoute exact path="/" component={MainPage} />
         <AuthRoute exact path="/login" component={LoginForm} />

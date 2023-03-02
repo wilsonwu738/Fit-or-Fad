535ee8bf49a253a2e01166f2dbc1dc1e93b024c9
Timothy Chang <tchang135@gmail.com>
2023-03-02 01:04:23 -0500

    Like component updated

--- frontend/src/components/Likes/Like.js
+++ frontend/src/components/Likes/Like.js
@@ -1,39 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { likePage, unlikePage } from "../../store/likes";
+import { likePage, deleteLike } from "../../store/likes";
 // import { fetchPage } from "../../store/pages";
 
 const LikePage = ({ pageId }) => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.session.user);
   const [liked, setLiked] = useState(false);
-
+  const currentStatus = user ? user.likedPage.includes(pageId) : false
   const page = useSelector((state) => state.pages)
 
-  useEffect(() => {
-    // debugger
-    if (page && page.liker && page.liker.includes(user._id)) {
-      setLiked(true);
-    } else {
-      setLiked(false);
-    }
-  }, [page, user]);
+  const handleLike = async () => {
+    dispatch(likePage(pageId))
+    setLiked(true)
+  }
 
-  const handleLikeClick = () => {
-    if (liked) {
-      dispatch(unlikePage(pageId, user._id));
-    } else {
-      dispatch(likePage(pageId, user._id));
-    }
-  
-    setLiked(!liked);
-  };
+  const handleUnlike = async () => {
+    dispatch(deleteLike( pageId ))
+    setLiked(false)
+  }
 
-  return (
-    <button onClick={handleLikeClick}>
-      {liked ? "Unlike" : "Like"}
-    </button>
-  );
+  if (liked) {
+    return  <button onClick={handleLike}>Like</button>
+   } else {
+    return <button onClick={handleUnlike}>Unlike</button>
+   }
 };
 
 export default LikePage;

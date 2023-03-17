9bbd77d89e842d8a8cec0dc06d74c356a59ef003
Timothy Chang <tchang135@gmail.com>
2023-03-17 13:40:02 -0400

    pages on profiles are now links

--- frontend/src/components/Pages/ProfileIndexItem.js
+++ frontend/src/components/Pages/ProfileIndexItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { clearPageErrors, fetchPages } from "../../store/pages";
+import { Link } from "react-router-dom";
 import "./ProfileIndexItem.css";
 
 const ProfileIndexItem = ({ page, userId }) => {
@@ -9,13 +10,15 @@ const ProfileIndexItem = ({ page, userId }) => {
 
   return (
     <>
-      <div id="item">
-        <img src={imageUrl} />
-        <div id="page-title">{title}</div>
-        <hr></hr>
-        <br></br>
-        <div id="page-description">{description}</div>
-      </div>
+      <Link className="profilePageItem"to={`/show/${page?._id}`}>
+        <div id="item">
+          <img src={imageUrl} />
+          <div id="page-title">{title}</div>
+          <hr></hr>
+          <br></br>
+          <div id="page-description">{description}</div>
+        </div>
+      </Link>
     </>
   );
 };
